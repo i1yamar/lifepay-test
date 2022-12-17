24bf17d996ebb8515ff0be293e63595bcb4672cb
i1yamar <marin-1998-4@mail.ru>
2022-12-17 17:23:20 +0700

    test
    test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Мобильный эквайринг с комиссией от 1,2%</title>
-    <link type="Image/x-icon" href="favicon.ico" rel="icon">
+    <link type="Image/x-icon" href="/favicon.ico" rel="icon">
     <link
       rel="stylesheet"
       type="text/css"
